e9c3f0fef8ecfdcfb2f1b160c19678264b7eef9f
Cole Peters <cole@colepeters.com>
2016-07-11 21:40:07 +0100

    RC1: Remove outdated comment

--- tasks/lint.js
+++ tasks/lint.js
@@ -2,7 +2,6 @@ var gulp = require('gulp')
 var postcss = require('gulp-postcss')
 
 // Lint source CSS using Stylelint
-// Rules are defined in config.js
 gulp.task('lint', function () {
   return gulp.src('./src/**/*.css')
   .pipe(postcss([
